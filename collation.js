72abf98de97dc8d59fb43b37217faeea31cfbe27
Dean Landolt <dean@deanlandolt.com>
2015-05-13 00:51:50 -0400

    Kill the buffertools stuffs

--- collation.js
+++ collation.js
@@ -3,55 +3,6 @@
 //
 var collation = require('typewise-core/collation')
 
-//
-// replace base bitwise comparison with faster native versions where available
-//
-try {
-  if (Buffer.compare) {
-    collation.bitwise = Buffer.compare
-  }
-  else {
-    try {
-      //
-      // attempt to use a faster native implementation from the buffertools lib
-      //
-      require('buffertools')
-      var _bitwise = collation.bitwise
-      var _bitwiseShortlex = shortlex.bitwise
-
-      //
-      // buffertools compare is shortlex
-      //
-      shortlex.bitwise = function (a, b) {
-        try {
-          return a.compare(b)
-        }
-        catch (e) {
-          return _bitwiseShortlex(a, b)
-        }
-      }
-
-      //
-      // bypass buffertools compare if lengths differ (it uses shortlex order)
-      //
-      collation.bitwise = function (a, b) {
-        // TODO: slice larger buffer and run through buffertools compare instead?
-        if (a.length !== b.length)
-          return _bitwise(a, b)
-
-        try {
-          return a.compare(b)
-        }
-        catch (e) {
-          return _bitwise(a, b)
-        }
-      }
-    }
-    catch (e) {}
-  }
-}
-catch (e) {}
-
 // TODO: set, map
 
 module.exports = collation
